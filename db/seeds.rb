e2dd6fc5e7ed442d8bfafbf3320c718bb49dd84f
n-lai <nlai@ucdavis.edu>
2016-06-24 11:22:51 -0700

    BenchBnb Phase 0 Rails Backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+romantic_bench = Bench.create(description: "Romantic Bench for Couples", lat: 37.752334, lng: -122.447551)
+princess_bench = Bench.create(description: "Bench for the princess", lat: 37.766898, lng: -122.468331)
+cat_bench = Bench.create(description: "Bench for Cats", lat: 37.766550, lng: -122.442705)
+dog_bench = Bench.create(description: "Bench for Dogs", lat: 37.772042, lng: -122.458262)
+annoying_family_bench = Bench.create(description: "Bench for families with annoying kids", lat: 37.777192, lng: -122.417343)
+smoker_bench = Bench.create(description: "Bench for smokers", lat: 37.759486, lng: -122.413027)
+messi_bench = Bench.create(description: "Bench for Messi because he's awesome", lat: 37.778113, lng: -122.511761)
