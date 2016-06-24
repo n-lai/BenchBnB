13099724307e0fca6c43c5c20e3bd49c9f9f92e0
n-lai <nlai@ucdavis.edu>
2016-06-24 11:37:52 -0700

    BenchBnb Phase 1 Rails Frontend

---
+++ frontend/bench_bnb.jsx
@@ -0,0 +1,8 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+document.addEventListener("DOMContentLoaded", function() {
+  ReactDOM.render(
+    document.getElementById('content')
+  );
+});
